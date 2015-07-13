0ebcf394e3be6f36c8b74fbc27d8ce051821d692
Chadwick Jones <chadwickdajones@gmail.com>
2015-07-13 16:15:14 -0700

    Added adding and started to try database stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
+  
 
+});
+
+var myDataRef = new Firebase('https://popping-inferno-6981.firebaseio.com/');
+
+var addNewPlace = function () {
+  	var name = $('#in-name').val();
+  	var place = $('#in-place').val();
+  	var type = $('#in-type').val();
+
+  	if (name != ''  && place != '' && type != '') {
+
+	  	// createDest(name, place, type);
+
+	  	$('#in-name').val('');
+	  	$('#in-place').val('');
+	  	$('#in-type').val('');
+
+	  	myDataRef.push({name: name, place: place, type: type});
+	}
+}
+
+var createDest = function (name, place, type) {
+	$('.list-container').append('<div class="list-item mdl-card mdl-shadow--2dp"><div class="list-text mdl-card__supporting-text">'
+		+ name + ', '
+		+ place + ' ('
+		+ type + ')'
+		+'</div></div>');
+}
+
+myDataRef.on('child_added', function(snapshot) {
+  var dest = snapshot.val();
+  createDest(dest.name, dest.place, dest.type);
 });
